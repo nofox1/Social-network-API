474967b50db561f8cbce6c610e73f2a89f4c7e46
NoFox <nfox598625@gmail.com>
2024-02-10 15:39:11 -0500

    modified updateUsers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -37,19 +37,28 @@ const usersController = {
       res.status(500).json(err);
     }
   },
-  updateUsers({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, {
-      new: true,
-      runValidators: true,
-    })
-      .then((userData) => {
-        if (!userData) {
-          res.status(404).json({ message: "No user found with this id!" });
-          return;
+  async updateUsers(req, res) {
+    try {
+      const userData = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        {
+          $set: req.body,
+        },
+        {
+          runValidators: true,
+          new: true,
         }
-        res.json(userData);
-      })
-      .catch((err) => res.json(err));
+      );
+
+      if (!userData) {
+        return res.status(404).json({ message: "No user with that id!" });
+      }
+
+      res.json(userData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
   deleteUsers({ params }, res) {
     User.findOneAndDelete({ _id: params.id })
