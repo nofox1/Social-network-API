b147301b36f05c09daeea08b705a2dd03e41ed90
NoFox <nfox598625@gmail.com>
2024-02-10 15:54:06 -0500

    fixed

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,7 +1,6 @@
 const { Thought, User } = require("../models");
 
 const thoughtController = {
-  // get all thoughts
   async getThoughts(req, res) {
     try {
       const thoughtData = await Thought.find().sort({ createdAt: -1 });
