f4ddd12063045af2e18d832141045e2e50c2c3ce
NoFox <nfox598625@gmail.com>
2024-02-10 14:58:10 -0500

    modified getAllUsers function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,19 +1,15 @@
 const { User, Thought } = require("../models");
 
 const usersController = {
-  getAllUsers(req, res) {
-    User.find({})
-      .populate({
-        path: "friends",
-        select: "-__v",
-      })
-      .select("-__v")
-      .sort({ _id: -1 })
-      .then((userData) => res.json(userData))
-      .catch((err) => {
-        console.log(err);
-        res.sendStatus(400);
-      });
+  async getAllUsers(req, res) {
+    try {
+      const userData = await User.find().select("-__v");
+
+      res.json(userData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
